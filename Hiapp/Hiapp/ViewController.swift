eccc047252bb43f0cf3636d55c1e119961bbf67c
vivek drakshapally <drakshapallyvivek31@gmail.com>
2021-08-26 15:01:39 -0500

    hiapp

---
+++ Hiapp/Hiapp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Hiapp
+//
+//  Created by Drakshapally,Vivek on 8/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var inputtext: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func submit(_ sender: Any) {
+        //Read the data from InputText
+               var ipText = inputtext.text!
+               //Assign it to DisplayLabel
+               label.text = "Hi, \(ipText)!"
+    }
+    
+}
+
